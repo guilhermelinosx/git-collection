b1c087232a48917738bf92187184c7aecbc96f5d
Guilherme Lino <guilhermelinosx@gmail.com>
2022-07-18 06:52:06 -0300

    dashboard edit styled

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
-import { Title, Form, Repos } from './styles'
+import { Title, Form, Repos, Error } from './styles'
 import { FiChevronRight } from 'react-icons/fi'
+import { api } from '../../services/api'
 import logo from '../../assets/logo.svg'
 
+interface GithubRepository {
+	full_name: string
+	description: string
+	owner: {
+		login: string
+		avatar_url: string
+	}
+}
+
 export const Dashboard = () => {
+	const [repos, setRepos] = React.useState<GithubRepository[]>([])
+	const [newRepo, setNewRepo] = React.useState('')
+	const [inputError, setInputError] = React.useState('')
+
+	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+		setNewRepo(event.target.value)
+	}
+
+	async function handleAddRepo(
+		event: React.FormEvent<HTMLFormElement>,
+	): Promise<void> {
+		event.preventDefault()
+
+		if (!newRepo) {
+			setInputError('Informe o username/repositório')
+			return
+		}
+
+		const response = await api.get<GithubRepository>(`repos/${newRepo}`)
+		const repository = response.data
+
+		setRepos([...repos, repository])
+		setNewRepo('')
+	}
+
 	return (
 		<>
 			<img src={logo} alt="Logo GitCollection" />
@@ -12,23 +47,27 @@ export const Dashboard = () => {
 				repositórios do <br />
 				GitHub
 			</Title>
-			<Form>
-				<input placeholder="username/repository" />
+			<Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+				<input placeholder="username/repository" onChange={handleInputChange} />
 				<button type="submit">Buscar</button>
 			</Form>
 
+			{inputError && <Error>{inputError}</Error>}
+
 			<Repos>
-				<a href="/repositories">
-					<img
-						src="https://avatars.githubusercontent.com/u/89226904?v=4"
-						alt="Repositório"
-					/>
-					<div>
-						<strong>guilhermelinosx/apisales</strong>
-						<p>Repositório do mini curso gratuito</p>
-					</div>
-					<FiChevronRight size={20} />
-				</a>
+				{repos.map(repository => (
+					<a href="/repositories" key={repository.full_name}>
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
+						<FiChevronRight size={20} />
+					</a>
+				))}
 			</Repos>
 		</>
 	)
